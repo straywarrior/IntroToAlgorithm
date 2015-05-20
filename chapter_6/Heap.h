0d7b8d78123739cf932a3202dcc06b85e88ad162
Feng Zhoutian <fengzhoutian@hotmail.com>
2015-05-20 22:36:35 +0800

    add heap

--- chapter_6/Heap.h
+++ chapter_6/Heap.h
@@ -1,4 +1,9 @@
 #include <exception>
+/*
+* The first index of element will be 1
+*
+*/
+
 class HeapOutOfBoundException : public std::exception{};
 
 template <typename T>
@@ -9,46 +14,48 @@ private:
 	int size;
 public:
 	BaseHeap(int _Capacity);
+	BaseHeap(T * src, int size);
 
 	virtual int push(T val) = 0;
 	virtual T pop() = 0;
+	virtual heapify(int i) = 0;
 
-	T parent(int i){
-		if (i < size)
-			return A[i >> 1];
-		else
-			throw HeapOutOfBoundException();
+	void buildHeap();
+
+	inline int parent(int i){
+		return (i >> 1)
 	}
-	T left(int i){
-		if (2 * i < size)
-			return A[i << 1];
-		else
-			throw HeapOutOfBoundException();
+	inline int left(int i){
+		return (i << 1);
 	}
-	T right(int i){
-		if (2 * i + 1 < size)
-			return A[(i << 1) + 1];
-		else
-			throw HeapOutOfBoundException();
+	inline int right(int i){
+		return (i << 1) + 1;
 	}
 };
 
 template <typename T>
-BaseHeap::BaseHeap(int _Capacity):capacity(_Capacity), size(0){
-	A = new T[capacity];
+BaseHeap<T>::BaseHeap(int _Capacity):capacity(_Capacity), size(0){
+	A = new T[capacity + 1];
 }
+
+template <typename T>
+BaseHeap<T>::BaseHeap(T * src, int size):capacity(size), size(size){
+	A = new T[capacity + 1];
+
+}
+
 
 template <typename T>
 class MaxHeap : public BaseHeap<T>{
 public:
 	int push(T val) override;
 	T pop() override;
-	void maxHeapify(int i);
+	void heapify(int i) override;
 };
 
 template <typename T>
 class MinHeap : public BaseHeap<T>{
 	int push(T val) override;
 	T pop() override;
-	void minHeapify(int i);
+	void heapify(int i) override;
 };
