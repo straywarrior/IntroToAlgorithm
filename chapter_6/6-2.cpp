4294f173f8ba8eb87b89dca2c72e6062b96588fd
Feng Zhoutian <fengzhoutian@hotmail.com>
2015-06-05 21:27:44 +0800

    Improve heapify()

--- chapter_6/6-2.cpp
+++ chapter_6/6-2.cpp
@@ -1,16 +1,17 @@
 /*
 * Author : StrayWarrior
-* Solution to 6.2-1, 6.2-2
+* Solution to 6.2-1, 6.2-2, 6.2-3
 * 
 */
 #include <iostream>
 #include "Heap.h"
 #include <time.h>
 
+
 int main()
 {
 	int n;
-	std::cout << "Input the number of test case: " << std::endl;
+	std::cout << "Input the length of test case: " << std::endl;
 	std::cin >> n;
 	int * A = new int[n];
 	srand(time(NULL));
@@ -20,7 +21,9 @@ int main()
 	MaxHeap<int> heap_max(A, n);
 	MinHeap<int> heap_min(A, n);
 	heap_max.buildHeap();
+	heap_min.buildHeap();
 	heap_max.print();
+	heap_min.printf();
 	
 	return 0;
 }
--- chapter_6/Heap.h
+++ chapter_6/Heap.h
@@ -7,6 +7,7 @@
 #define _HEAP_H_
 #include <string.h>
 #include <iostream>
+#include <math.h>
 
 class HeapOutOfBoundException : public std::exception{};
 
@@ -29,8 +30,22 @@ public:
 	virtual T pop() = 0;
 	virtual void heapify(int i) = 0;
 
-	void buildHeap();
-	void print();
+	void buildHeap(){
+		for (int i = (size >> 1); i > 0; --i)
+			heapify(i);
+	}
+	void print(){
+		for (int i = 1; i <= size; i++)
+			std::cout << A[i] << " ";
+		std::cout << std::endl;
+	}
+	void printf(){
+		for (int i = 1; i <= exp2(round(log2(size))); i = i << 1){
+			for (int j = i; j < (i << 1) && j <= size; j++)
+				std::cout << A[j] << " ";
+			std::cout << std::endl;
+		}
+	}
 
 	inline int parent(int i){
 		return (i >> 1);
@@ -66,20 +81,6 @@ public:
 
 
 template <typename T>
-void BaseHeap<T>::print(){
-	for (int i = 1; i <= size; i++)
-		std::cout << A[i] << " ";
-	std::cout << std::endl;
-}
-
-template <typename T>
-void BaseHeap<T>::buildHeap(){
-	for (int i = (size >> 1); i > 0; --i){
-		heapify(i);
-	}
-}
-
-template <typename T>
 int MaxHeap<T>::push(T val){
 	return 0;
 }
@@ -98,6 +99,11 @@ T MinHeap<T>::pop(){
 	return A[1];
 }
 
+
+/*
+* Recursive Edition of Heapify-function
+*/
+#ifdef _HEAP_HEAPIFY_RECUR_
 template <typename T>
 void MaxHeap<T>::heapify(int i)
 {
@@ -106,7 +112,7 @@ void MaxHeap<T>::heapify(int i)
 	int max;
 	if (l > size && r > size)
 		return;
-	if ((l <= size) && (A[l] > A[i]))
+	if ((l <= size) && (A[l] > A[i])) //l is sure not greater than size..but isn't r
 		max = l;
 	else
 		max = i;
@@ -141,4 +147,74 @@ void MinHeap<T>::heapify(int i)
 		heapify(min);
 	}
 }
+#else
+
+/*
+* Iterative Edition of Heapify-function
+*/
+
+template <typename T>
+void MaxHeap<T>::heapify(int i)
+{
+	int i_cur = i;
+	int l, r, max;
+
+	while (i_cur <= size >> 1){
+		l = left(i_cur);
+		r = right(i_cur);
+		if (l > size && r > size)
+			return;
+		if (A[l] > A[i_cur])
+			max = l;
+		else
+			max = i_cur;
+		if (r <= size && A[r] > A[max])
+			max = r;
+		if (max != i_cur){
+			T tmp = A[i_cur];
+			A[i_cur] = A[max];
+			A[max] = tmp;
+			i_cur = max;
+		}
+		else{
+			break;
+		}
+	}
+}
+
+template <typename T>
+void MinHeap<T>::heapify(int i)
+{
+	int i_cur = i;
+	int l, r, min;
+
+	while (i_cur <= size >> 1){
+		l = left(i_cur);
+		r = right(i_cur);
+		if (l > size && r > size)
+			return;
+		if (A[l] < A[i_cur])
+			min = l;
+		else
+			min = i_cur;
+		if (r <= size && A[r] < A[min])
+			min = r;
+		if (min != i_cur){
+			T tmp = A[i_cur];
+			A[i_cur] = A[min];
+			A[min] = tmp;
+			i_cur = min;
+		}
+		else{
+			break;
+		}
+	}
+}
+
+#endif
+
+
+
+
+
 #endif
